81ac2b2ad4f5b28cc95546714e68a6c6e05ce244
Melih Taşçı <melihhtasci@gmail.com>
2022-10-16 16:59:41 +0300

    Parameters changed and rate bug fixed.
    I had made an error while converting between currencies. I fixed this. I used Argument instead of Option

--- Exchange.cs
+++ Exchange.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace exchange_cli
+﻿namespace exchange_cli
 {
     public class Exchange
     {
@@ -12,13 +6,28 @@ namespace exchange_cli
         public bool success { get; set; }
         public string _base { get; set; }
         public string date { get; set; }
+        public Info info { get; set; }
         public Rates rates { get; set; }
+        public Query query { get; set; }
+        public float? result { get; set; }
     }
 
     public class Motd
     {
         public string msg { get; set; }
         public string url { get; set; }
+    }
+
+    public class Query
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public float amount { get; set; }
+    }
+
+    public class Info
+    {
+        public float? rate { get; set; }
     }
 
     public class Rates
--- Helper.cs
+++ Helper.cs
@@ -1,26 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace exchange_cli
 {
     public class Helper
     {
-        public static string UrlBuilder(string fromExchange="", string amount="")
+        public static string UrlBuilder(string from = "", string to = "", string amount = "")
         {
-            string url = string.Empty;
+            StringBuilder stringBuilder = new();
 
-            if (!string.IsNullOrEmpty(fromExchange))
-                url += Consts.apiUrl + "?base=" + fromExchange;
+            if (!string.IsNullOrEmpty(from) && !string.IsNullOrEmpty(to))
+            {
+                stringBuilder.Append(Consts.fromUrl + from);
+                stringBuilder.Append(Consts.toUrl + to);
+            }
             else
-                url += Consts.apiUrl + Consts.baseTry;
+            {
+                stringBuilder.Append(Consts.apiUrl + Consts.baseTry);
+            }
 
             if (!string.IsNullOrEmpty(amount))
-                url += "&amount=" + amount;
+            { 
+                stringBuilder.Append("&amount=" + amount);
+            }
 
-            return url;
+            return stringBuilder.ToString();
+        }
+
+        public static void Validations(string from, string to, string amount)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(from))
+                    if (!Regex.IsMatch(from, "^[a-zA-Z]+$") || from.Length != 3)
+                        throw new Exception("FROM value must be only letter and length must be 3");
+
+                if (!string.IsNullOrEmpty(to))
+                    if (!Regex.IsMatch(to, "^[a-zA-Z]+$") || from.Length != 3)
+                        throw new Exception("TO value must be only letter and length must be 3");
+
+                if (!string.IsNullOrEmpty(amount))
+                {
+                    // regex number and number-dot check
+                    // throw new Exception("AMOUNT value include just number or number with dot. Example : 23.1");
+                }
+
+                ArgumentRules(from, to);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+
+        }
+
+        public static void ArgumentRules(string from, string to) 
+        {
+            if (!string.IsNullOrEmpty(from) && string.IsNullOrEmpty(to))
+            {
+                Console.WriteLine("You must specify to currency to convert it.");
+                Console.WriteLine($"Convert from {from.ToUpper()} to what?!");
+                MakeLine();
+                Environment.Exit(0);
+            }
         }
 
         public static void MakeLine()
